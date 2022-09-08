309f7d286bb88d94eda4c6be300dbc23864a5136
icodo98 <rsb98759@gmail.com>
2022-09-08 09:32:29 +0900

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -7,6 +7,8 @@ public class Collatz {
     public static int nextNumber(int n) {
         if (n  == 128) {
             return 1;
+        } else if (n == 1) {
+            return 1;
         } else if ((n % 2) != 0) {
             //if n is odd, next number is 3n +1
             return 3 * n + 1;

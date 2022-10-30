3ad16c652ef742d1dacec68b95007ebe22357518
icodo98 <rsb98759@gmail.com>
2022-10-31 03:00:35 +0900

    "Done with saveDog and fromFile."

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,13 +1,14 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
  * @author TODO
 */
-public class Dog { // TODO
+public class Dog implements Serializable{
 
     /** Folder that dogs live in. */
     static final File DOG_FOLDER = Utils.join(CapersRepository.CAPERS_FOLDER,"dogs/");
@@ -38,8 +39,8 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        File f = new File(DOG_FOLDER,name);
+        return readObject(f,Dog.class);
     }
 
     /**
@@ -55,7 +56,14 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        File f = new File(DOG_FOLDER,this.name);
+        try{
+            f.createNewFile();
+        }
+        catch (IOException e){
+
+        }
+        writeObject(f, this);
     }
 
     @Override

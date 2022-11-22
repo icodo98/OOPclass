05509897390cbcf170be092386211393df8d997d
icodo98 <rsb98759@gmail.com>
2022-11-22 09:55:41 +0900

    Deleted peer code

--- lab5/lab3/Launcher.java
+++
@@ -1,6 +0,0 @@
-public class Launcher {
-    public static void main(String[] arg){
-        warlus w = new warlus(100, 4.5);
-        System.out.println(w.tusksize);
-    }
-}
--- lab5/lab3/warlus.java
+++
@@ -1,9 +0,0 @@
-public class warlus {
-    public int weight;
-    public double tusksize;
-
-    public warlus(int w, double ts) {
-        weight = w;
-        tusksize = ts;
-    }
-}
